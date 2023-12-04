2e8f44f8224d1dfa25ae50383237a86d305ea2cc
TheoAlix <62751931+TheoAlix@users.noreply.github.com>
2023-12-04 22:55:06 +0100

    Update GameCLI.java

--- pieces/GameCLI.java
+++ pieces/GameCLI.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+
 public class GameCLI{
 
     PlateauPuzzle board;
@@ -31,6 +32,11 @@ public class GameCLI{
     }
 
     public Piece makeRandomPiece(int longueur, int orientation){
+        /**
+         * Fonction qui permet de créer une pièce aléatoire
+         * @param longueur est la longueur de la pièce
+         * @param orientation est l'orientation de la pièce
+         */
         int indexP = new Random().nextInt(this.listPiece.length);
         if (indexP == 1){
             return new L(longueur,orientation);
@@ -44,96 +50,149 @@ public class GameCLI{
         return new Rec(longueur,orientation);
     }
 
+    @Override
+    public String toString(){
+        return this.board.toString();
+    }
+
+    public int score(){
+        // Méthode qui calcul le score de l'état actuel de la partie (Le but étant de faire le plus petit score possible)
+        int minXGauche = 10000000;
+        int minYGauche = 10000000;
+
+        int maxXDroit = 0;
+        int maxYDroit = 0;
+        for(Piece p : this.board.map){
+            // On parcours toutes les pièces 
+            int pX = p.x;
+            int pY = p.y;
+
+            if(pX < minXGauche){
+                // On cherche la pièce la plus en haut à gauche possible
+                minXGauche = pX;
+            }
+
+            if(pY < minYGauche){
+                minYGauche = pY;
+            }
+
+            if(pX > maxXDroit){
+                // On cherche la pièce la plus en bas à droite possible
+                maxXDroit = pX;
+            }
+
+            if(pY > maxYDroit){
+                maxYDroit = pY;
+            }
+        }
+
+        int longueur = maxXDroit - minXGauche;
+        int largeur = maxYDroit - minYGauche;
+
+
+        System.out.println(minXGauche);
+        System.out.println(minYGauche);
+
+        System.out.println();
+
+        System.out.println(maxXDroit);
+        System.out.println(maxYDroit);
+
+        System.out.println();
+
+        System.out.println(longueur);
+        System.out.println(largeur);
+
+        return longueur*largeur;
+    }
+
     public Piece trouverPiece(int x, int y){
+        /**
+         * Fonction qui permet de trouver une pièce à partir de son centre
+         * @param x est l'abscisse du centre de la pièce
+         * @param y est l'ordonnée du centre de la pièce
+         */
         // fonction qui renvoie la piece qui a pour centre x,y et renvoie une pièce de longueur 0 sinon
-        Piece error = new L(0,0);
-        List<Integer> res = new ArrayList<>();
-        res.add(x);
-        res.add(y);
+        Piece error = new L(0,0); 
         for (Piece p : this.board.map){
-            if(p.centreGrille() == res){
+            System.out.println(p.x);
+            System.out.println(p.y); 
+            System.out.println(p.x+ p.centreGrille().get(0));
+            System.out.println(p.y+ p.centreGrille().get(1));
+            if(p.x == x && p.y== y){
                 return p;
             }
         }
         return error;
     }
 
-    public boolean turnRight(int x, int y){
+    public boolean turn(int x, int y, String sens){
+        /**
+         * Fonction qui permet de tourner une pièce
+         * @param x est l'abscisse du centre de la pièce
+         * @param y est l'ordonnée du centre de la pièce
+         * @param sens est le sens de rotation de la pièce (R = droite, L = gauche)
+         */
+
         // x et y doivent être le centre de la pièce (largeur/2,hauteur/2)
         Piece p = this.board.deletePiece(x,y);
         if (p.longueur != 0){
-            // la pièce p est la pièce qui a été supprimé du board
-            p.rotateRight();
+            if(sens == "R"){
+                // la pièce p est la pièce qui a été supprimé du board
+                p.rotateRight();
+                
+            }else{
+                // la pièce p est la pièce qui a été supprimé du board
+                p.rotateLeft();
+            }
+
             if(this.board.ajouterPiece(p,x,y)){
                 // La pièce a été ajouté en etant tournée
                 return true;
             }
-            p.rotateLeft();
+            // La pièce n'a pas pu être ajoutée
+            if(sens == "R"){p.rotateLeft();}
+            else{p.rotateRight();}
             this.board.ajouterPiece(p,x,y); // On remet la pièce à l'endroit initial
         }
         return false;
     }
 
-    @Override
-    public String toString(){
-        return this.board.toString();
-    }
-
-    // public int score(){
-    //     // Méthode qui calcul le score de l'état actuel de la partie (Le but étant de faire le plus petit score possible)
-    //     int minXGauche;
-    //     int minYGauche;
-
-    //     int maxXDroit;
-    //     int maxYDroit;
-    //     for(Piece p : this.board.map){
-
-    //     }
-
-    //     int longueur = maxXDroit - minXGauche;
-    //     int largeur = maxYDroit - maxYGauche;
-    //     return longueur*largeur;
-    // }
-    public boolean move(Integer x, Integer y, int newX, int newY){
+    public boolean turnLeft(int x, int y){
+        /**
+         * Fonction qui permet de tourner une pièce à gauche
+         * @param x est l'abscisse du centre de la pièce
+         * @param y est l'ordonnée du centre de la pièce
+         */
         // x et y doivent être le centre de la pièce (largeur/2,hauteur/2)
         Piece p = this.board.deletePiece(x,y);
-        p.setPosition(newX, newY);
-        if(this.board.ajouterPiece(p,newX,newY)){
-            return true;
+        if (p.longueur != 0){
+            
         }
-        this.board.ajouterPiece(p,x,y);
         return false;
     }
 
     public void jouer(){
+        /**
+         * Fonction qui permet de jouer au jeu
+         * Ne prends rien en entrée et ne renvoie rien, uniquement des affichages et des inputs de l'utilisateur
+         */
         Scanner scanner = new Scanner(System.in);
-        int tailleGrille = 30;
-        int nbPieces =12;
+        int tailleGrille = 10;
+        int nbPieces = 1;
         GameCLI game = new GameCLI(tailleGrille,tailleGrille,nbPieces);
         ViewConsole view = new ViewConsole(game.board);
         boolean running = true;
-        Piece pi = new I(4,0);
-        game.board.ajouterPiece(pi,1,4);
-        game.board.modeleMisAJour();
-        List<Integer> res = pi.centreGrille();
-        System.out.println( "Move est : " + game.move(res.get(0),res.get(1),2,4));
-        game.board.modeleMisAJour();
-        for (Piece p : game.board.map){
-            List<Integer> tmp = p.centreGrille();
-            System.out.println(("Centre :" + String.valueOf(tmp.get(0)) + String.valueOf(tmp.get(1))));
-        }
         while(running){
-            
-            game.board.modeleMisAJour();
-
 
             Piece tmp;
             int x = 0;
             int y = 0;
             do{ // Selection de la pièce
-                System.out.println("Entrez le x de la pièce choisis (x étant celui du centre de la pièce)");
+                game.board.modeleMisAJour();
+                System.out.println("Entrez le x (de la case la plus à gauche) de la pièce choisis (x étant l'abscisse)");
                 x = Integer.valueOf(scanner.nextLine());
-                System.out.println("Entrez le y de la pièce choisis (y étant celui du centre de la pièce)");
+                System.out.println("Entrez le y (de la case la plus à gauche) de la pièce choisis (y étant l'ordonnée)");
                 y = Integer.valueOf(scanner.nextLine());
                 tmp = game.trouverPiece(x,y);
             }while(tmp.longueur == 0);
@@ -157,15 +216,16 @@ public class GameCLI{
             }
             if(choix == 2){
                 // On essaie de tourner la piece choisis à droite
-                if(game.turnRight(x,y)==false){
+                if(game.turn(x,y,"R")==false){
                     System.out.println("Impossible de tourner cette pièce vers la droite");
                 }
             }
-            if(choix == 2){
+            if(choix == 3){
                 // On essaie de tourner la piece choisis à gauche
-                System.out.println("Pas implémenté");
+                if(game.turn(x,y,"L")==false){
+                    System.out.println("Impossible de tourner cette pièce vers la Gauche");
+                }
             }
         }
     }
-  
 }
