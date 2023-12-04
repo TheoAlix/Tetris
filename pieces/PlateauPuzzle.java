517f2dfc1951827f5026ce51e012ab6e1fd9c416
TheoAlix <62751931+TheoAlix@users.noreply.github.com>
2023-12-04 20:03:51 +0100

    Update PlateauPuzzle.java

--- pieces/PlateauPuzzle.java
+++ pieces/PlateauPuzzle.java
@@ -14,6 +14,9 @@ public class PlateauPuzzle{
     }
 
     public boolean ajouterPiece(Piece piece, int x, int y) {
+        if (x < 0 || y < 0 || x + piece.largeurActuelle() > this.largeur || x + piece.hauteurActuelle() > this.largeur || y + piece.hauteurActuelle() + piece.longueur > this.hauteur || y + piece.largeurActuelle() + piece.longueur > this.hauteur ||x + 1 > this.largeur || y + 1 > this.hauteur) {
+            return false;
+        }
         for (Piece p : map) {
             if (intersect(piece, x, y, p)) {
                 return false; 
@@ -57,8 +60,8 @@ public class PlateauPuzzle{
 
         if (x1 <= x2Max && x1Max >= piece2.x && y1 <= y2Max && y1Max >= piece2.y) {
             if (this.largeur < x2Max + 1 && this.largeur < x1Max + 1 && this.hauteur < y2Max + 1 && this.hauteur < y1Max + 1) {
-                for (int i = Math.max(x1, piece2.x); i <= Math.min(x1Max, x2Max)-1; i++) {
-                    for (int j = Math.max(y1, piece2.y); j <= Math.min(y1Max, y2Max)-1; j++) {
+                for (int i = Math.max(x1, piece2.x); i <= Math.min(x1Max, x2Max); i++) {
+                    for (int j = Math.max(y1, piece2.y); j <= Math.min(y1Max, y2Max); j++) {
                         if (piece1.estOccupee(i - x1, j - y1) && piece2.estOccupee(i - piece2.x, j - piece2.y)){
                             return true;
                         }
@@ -82,45 +85,76 @@ public class PlateauPuzzle{
 
     @Override
     public String toString() {
-        // TODO : Créer une grille de caractères représentant le plateau de jeu puis retourner la chaîne de caractères la représentant.
-        String[][] grille = new String[hauteur][largeur];
+        // TODO : CrÃ©er une grille de caractÃ¨res reprÃ©sentant le plateau de jeu puis retourner la chaÃ®ne de caractÃ¨res la reprÃ©sentant.
+        String[][] grille = new String[hauteur+1][largeur+1];
 
-        for (int i = 0; i < hauteur; i++) {
-            for (int j = 0; j < largeur; j++) {
-                grille[i][j] = "□ ";
+        for (int i = 0; i < hauteur+1; i++) {
+            for (int j = 0; j < largeur+1; j++) {
+                grille[i][j] = " .";
             }
         }
-
-        // Placer chaque pièce dans la grille
+        // Placer chaque piÃ¨ce dans la grille
         for (Piece piece : map) {
 
             int[] ligne = piece.indexLigneActu();
             int[] col = piece.indexColActu();
+            int centreX = piece.centreGrille().get(0);
+            int centreY = piece.centreGrille().get(1);
             if (piece.orientation % 2 == 0){
-                for (int i = 0; i < (piece.tab.size()-1); i++) {
-                    for (int j = 0; j < (piece.tab.get(0).size()-1); j++) {
+                for (int i = 0; i < piece.tab.size(); i++) {
+                    for (int j = 0; j < piece.tab.get(0).size(); j++) {
                         if(piece.tab.get(col[i]).get(ligne[j])) {
-                            grille[piece.y + i][piece.x + j] = "■ ";
+                            if(i == centreX && j == centreY){
+                                grille[piece.y + i + 1][piece.x + j + 1] = " O";
+                            }else{
+                                grille[piece.y + i+1][piece.x + j+1] = " X";
+                            }
                         }
                     }
                 }
             }
             else{
-                for (int i = 0; i < (piece.tab.get(0).size()-1); i++) {
-                    for (int j = 0; j < (piece.tab.size()-1); j++) {
+                for (int i = 0; i < piece.tab.get(0).size(); i++) {
+                    for (int j = 0; j < piece.tab.size(); j++) {
                         if(piece.tab.get(ligne[j]).get(col[i])){
-                            grille[piece.y + i][piece.x + j] = "■ ";
+                            grille[piece.y + i+1][piece.x + j+1] = " X";
                         }
                     }
                 }
             }
         }
 
-        // Convertir la grille en chaîne de caractères
+        // Convertir la grille en chaÃ®ne de caractÃ¨res
         StringBuilder result = new StringBuilder();
-        for (int i = 0; i < hauteur; i++) {
-            for (int j = 0; j < largeur; j++) {
-                result.append(grille[i][j]);
+        for (int i = 0; i < hauteur+1; i++) {
+            for (int j = 0; j < largeur+1; j++) {
+                if(i == 0){
+                    if(j>0 && j<=10){
+                        result.append(String.valueOf(j-1));
+                        result.append("  ");
+                    }else if (j>0){
+                        result.append(String.valueOf(j-1));
+                        result.append(" ");
+                    }
+                    if (j==0){
+                        result.append("__| ");
+                    }
+                }
+                else if(j == 0){
+                    if(i>0){
+                        result.append(String.valueOf(i-1));
+                    }else{
+                        result.append(" ");
+                    }
+                    if(i <= 10){
+                        result.append(" ");
+                    }
+                    result.append("|");
+                }
+                else{
+                    result.append(grille[i][j]);
+                    result.append(" ");
+                }
             }
             result.append('\n');
         }
@@ -128,6 +162,7 @@ public class PlateauPuzzle{
         return result.toString();
     
     }
+
     public static void main(String[] args){
         Piece p1 = new T(7,1);
         Piece p2 = new T(4,0);
